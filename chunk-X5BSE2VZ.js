5713b5deeb158a67e8acf5c7b7190547aebd2b74
Temporary User <temp@example.com>
2025-06-04 17:21:33 -0500

    Auto-generated commit

---
+++ chunk-X5BSE2VZ.js
@@ -0,0 +1,1 @@
+var s={baseUrl:"https://64a6f5bc096b3f0fcc80e3fa.mockapi.io/api/v1",endpoints:{usuarios:"/usuarios",alumnos:"/alumnos",cursos:"/cursos",inscripciones:"/inscripciones"}};export{s as a};
